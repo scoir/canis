7c8b869d4ecd804502f39f1984b3309b1ed95268
Kevin Griffin <griffin.kev@gmail.com>
2020-09-18 01:26:19 -0400

    dont't thing we use these

--- cmd/verifier/main.go
+++ cmd/verifier/main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/hyperledger/aries-framework-go/pkg/framework/aries/defaults"
 	ariescontext "github.com/hyperledger/aries-framework-go/pkg/framework/context"
 	"github.com/pkg/errors"
+	mongodbstore "github.com/scoir/aries-storage-mongo/pkg/storage"
 	goji "goji.io"
 	"goji.io/pat"
 
