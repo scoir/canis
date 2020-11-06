ed13a4e9ddc7ea7ff3c1a2aa83a5c66ad1ba03da
Phil Feairheller <pfeairheller@gmail.com>
2020-11-06 10:54:12 -0500

    Initial connection to IBM testing
    Signed-off-by: Phil Feairheller <pfeairheller@gmail.com>

---
+++ cmd/ibm-verify-creds/main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"image"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/hyperledger/indy-vdr/wrappers/golang/identifiers"
+	"github.com/makiuchi-d/gozxing"
+	qr "github.com/makiuchi-d/gozxing/qrcode"
+	"github.com/mr-tron/base58"
+	"github.com/skip2/go-qrcode"
+)
+
+const (
+	URL               = "https://agency.ibmsecurity.verify-creds.com"
+	Account           = "phil@scoir.com"
+	AccountPassword   = "yc086z4wj8"
+	AgentName         = "ibm-test-agent-for-canis"
+	AgentPassword     = "canispw"
+	DefaultAgentSeed  = "1111111111111111111111IBMInterop"
+	DefaultLedgerName = "sovrin.staging"
+)
+
+type newAgentRequest struct {
+	ID                string `json:"id"`
+	Password          string `json:"password"`
+	Seed              string `json:"seed"`
+	DefaultLedgerName string `json:"default_ledger_name"`
+}
+
+func main() {
+
+	deleteAgent()
+	createAgent()
+	generateInvitation()
+}
+
+func deleteAgent() {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/api/v1/agents/%s", URL, AgentName), nil)
+	if err != nil {
+		log.Fatalln("unexpected error creating delete agent request", err)
+	}
+	req.SetBasicAuth(Account, AccountPassword)
+
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatalln("unexpected error calling delete agent", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("response code %d from delete agent\n", resp.StatusCode)
+		return
+	}
+
+	log.Printf("agent %s successfully deleted\n", AgentName)
+}
+
+func createAgent() {
+	agent := &newAgentRequest{
+		ID:                AgentName,
+		Password:          AgentPassword,
+		Seed:              DefaultAgentSeed,
+		DefaultLedgerName: DefaultLedgerName,
+	}
+
+	bits, err := json.Marshal(agent)
+	if err != nil {
+		log.Fatalln("unexpected error marshalling create agent request", err)
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/agents", URL), bytes.NewBuffer(bits))
+	if err != nil {
+		log.Fatalln("unexpected error creating new agent request", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(Account, AccountPassword)
+
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatalln("unexpected error calling new agent", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("response code %d from new agent\n", resp.StatusCode)
+		return
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln("unexpected error reading invitation body", err)
+	}
+
+	fmt.Println(string(b))
+
+	var pubkey ed25519.PublicKey
+	var privkey ed25519.PrivateKey
+	privkey = ed25519.NewKeyFromSeed([]byte(DefaultAgentSeed))
+	pubkey = privkey.Public().(ed25519.PublicKey)
+	did, err := identifiers.CreateDID(&identifiers.MyDIDInfo{PublicKey: pubkey, Cid: true, MethodName: "sov"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("New Agent DID:", did.String())
+	fmt.Println("New Agent Verkey:", did.AbbreviateVerkey())
+	fmt.Println("Place These in Wallet:")
+	fmt.Println("Public:", base58.Encode(pubkey))
+	fmt.Println("Private:", base58.Encode(privkey))
+	log.Printf("agent %s successfully created\n", AgentName)
+}
+
+func generateInvitation() {
+	buf := bytes.NewBuffer([]byte("{}"))
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/invitations", URL), buf)
+	if err != nil {
+		log.Fatalln("unexpected error creating invitation request", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(AgentName, AgentPassword)
+
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatalln("unexpected error calling get invitation", err)
+	}
+	m := map[string]interface{}{}
+	err = json.NewDecoder(resp.Body).Decode(&m)
+	if err != nil {
+		log.Fatalln("unexpected error decoding invitation request", err)
+	}
+
+	inviteURL := m["url"].(string)
+
+	fname := "./ibm-verify-cred-invite.png"
+	err = qrcode.WriteFile(inviteURL, qrcode.Medium, 256, fname)
+	if err != nil {
+		log.Fatalln("unexpected error generating QR code", err)
+	}
+
+}
+
+func encode() {
+	resp, err := http.Get("http://local.scoir.com:7779/agents/hogwarts/invitation/subject")
+	if err != nil {
+		log.Fatalf("Error requesting invitation from issuer: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	m := map[string]interface{}{}
+	err = json.NewDecoder(resp.Body).Decode(&m)
+	if err != nil {
+		log.Fatalln("unexpected error decoding invitation request", err)
+	}
+
+	b := m["invitation"].(string)
+
+	ci := base64.URLEncoding.EncodeToString([]byte(b))
+	str := fmt.Sprintf("http://192.168.86.30/?c_i=%s", ci)
+
+	fmt.Println(b)
+
+	fname := "./invite.png"
+	err = qrcode.WriteFile(str, qrcode.Medium, 256, fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func decode() {
+	imgdata, err := ioutil.ReadFile("./verity-invite.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	img, _, err := image.Decode(bytes.NewReader(imgdata))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	bmp, _ := gozxing.NewBinaryBitmapFromImage(img)
+
+	// decode image
+	qrReader := qr.NewQRCodeReader()
+	result, _ := qrReader.Decode(bmp, nil)
+
+	fmt.Println(result)
+}
--- go.mod
+++ go.mod
@@ -4,15 +4,14 @@ go 1.14
 
 replace github.com/hyperledger/aries-framework-go => github.com/pfeairheller/aries-framework-go v0.1.4-0.20200918145734-b0d185a4e869
 
-replace github.com/hyperledger/indy-vdr/wrappers/golang => github.com/pfeairheller/indy-vdr/wrappers/golang v0.0.1
+replace github.com/hyperledger/indy-vdr/wrappers/golang => ../indy-vdr/wrappers/golang
 
 require (
-	bou.ke/staticfiles v0.0.0-20190225145250-827d7f6389cd // indirect
 	github.com/btcsuite/btcutil v1.0.1
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.4.2
-	github.com/golangci/golangci-lint v1.31.0 // indirect
+	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/tink/go v1.4.0-rc2.0.20200807212851-52ae9c6679b2
 	github.com/google/uuid v1.1.1
@@ -21,14 +20,19 @@ require (
 	github.com/hyperledger/aries-framework-go v0.1.4
 	github.com/hyperledger/indy-vdr/wrappers/golang v0.0.0-20201031155907-5f437d26ed71
 	github.com/hyperledger/ursa-wrapper-go v0.0.0-20201020141813-07eecfdbb801
+	github.com/klauspost/compress v1.10.10 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/makiuchi-d/gozxing v0.0.0-20200903113411-25f730ed83da
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/mr-tron/base58 v1.2.0
 	github.com/mwitkow/grpc-proxy v0.0.0-20181017164139-0f1106ef9c76
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/onsi/ginkgo v1.13.0 // indirect
 	github.com/piprate/json-gold v0.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/rs/cors v1.7.0
 	github.com/scoir/aries-storage-mongo v0.0.0-20200924155006-8a599355491e
+	github.com/sirupsen/logrus v1.6.0 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5
@@ -37,16 +41,15 @@ require (
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	github.com/unidoc/unipdf/v3 v3.10.1
-	github.com/vektra/mockery v1.1.2 // indirect
 	go.mongodb.org/mongo-driver v1.4.1
 	goji.io v2.0.2+incompatible
 	golang.org/x/net v0.0.0-20200822124328-c89045814202
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	golang.org/x/sys v0.0.0-20200812155832-6a926be9bd1d // indirect
-	golang.org/x/tools v0.0.0-20200904140424-93eecc3576be // indirect
 	google.golang.org/genproto v0.0.0-20201007142714-5c0e72c5e71e
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	k8s.io/client-go v0.17.0
 	k8s.io/utils v0.0.0-20200603063816-c1c6865ac451 // indirect
 	nhooyr.io/websocket v1.8.3
